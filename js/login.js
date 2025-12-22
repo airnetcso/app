6ad07e8a5238a3780bad7fd977de68ba9f4c35d4
airnetcso <airnetcso@gmail.com>
2025-12-23 06:50:51 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,30 @@
-alert("login.js KELOAD");
+const users = [
+  { username: "andi", password: "1234" },
+  { username: "budi", password: "5678" },
+  { username: "siti", password: "1111" }
+];
+
+// kalau sudah login, langsung ke soal
+if (localStorage.getItem("login") === "true") {
+  location.href = "index.html";
+}
+
+function login() {
+  const u = document.getElementById("username").value.trim();
+  const p = document.getElementById("password").value.trim();
+
+  if (!u || !p) {
+    alert("Username dan password wajib diisi");
+    return;
+  }
+
+  const user = users.find(x => x.username === u && x.password === p);
+  if (!user) {
+    alert("Username atau password salah");
+    return;
+  }
+
+  localStorage.setItem("login", "true");
+  localStorage.setItem("nama", u);
+  location.href = "index.html";
+}
